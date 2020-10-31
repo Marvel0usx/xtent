d018f860b65fc212fbae5fe2d3bbc4d3f62eba5e
Harry Wang <marvel0usx@outlook.com>
2020-10-30 21:54:36 -0400

    [dev] runit.sh

--- a1b/runit.sh
+++ a1b/runit.sh
@@ -1,29 +1,22 @@
+make
 truncate -s 10M image
-./mkfs.a1fs -i 128 image
-mkdir test_root
-./a1fs image ./test_root
-
-cd ./test_root
+./mkfs.a1fs -i 128 image -f
+./a1fs ./image /tmp/wangh278
+cd /tmp/wangh278
 ls
-touch file1
-mkdir dir1
-cd dir1
-mkdir subdir1
+touch ./file1
+mkdir ./dir1
+cd ./dir1
+mkdir ./subdir1
 cd ..
-echo hello world! > file2
+echo hello world! > ./file2
 ls -lsa
-cat file2
-echo this assignment is really hard! >> file2
-cat file2 > file3
-cat file3
+cat ./file2
+echo this assignment is really hard! >> ./file2
+cat ./file2 > ./file3
+cat ./file3
 ls -lsa
-rm -f file3
+rm -f ./file3
 cd ..
-fusermount -u ./test_root
-
-./a1fs image ./test_root
-rm -rf .
-ls -lsa
-cd ..
-fusermount -u ./test_root
+fusermount -u /tmp/wangh278
 
